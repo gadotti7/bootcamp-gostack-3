949149ac35fbacff5a6248b7fa34fde7c1500428
Qualix <maateusgadotti@gmail.com>
2020-02-06 11:48:04 -0300

    Styled Components and styling forms

--- mod6/src/pages/Main/index.js
+++ mod6/src/pages/Main/index.js
@@ -1,8 +1,22 @@
 import React from 'react';
-import { View } from 'react-native';
 
-// import { Container } from './styles';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+import { Container, Form, Input, Submit } from './styles';
 
 export default function Main() {
-  return <View />;
+  return (
+    <Container>
+      <Form>
+        <Input
+          autoCorrect={false}
+          autoCapitalize="none"
+          placeholder="Adicionar usuário"
+        />
+        <Submit>
+          <Icon name="add" size={20} color="#FFF" />
+        </Submit>
+      </Form>
+    </Container>
+  );
 }
